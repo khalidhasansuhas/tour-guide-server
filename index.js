937b3544ab02a231799138e48fa878254078d534
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-11-09 16:52:36 +0600

    connected to the database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,12 @@ const port = process.env.PORT || 5000;
 //middlewares
 app.use(cors());
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d5moryc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', (req, res) => {
     res.send('tour guide server is running')
